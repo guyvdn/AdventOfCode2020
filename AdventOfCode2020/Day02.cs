3c7d2a4ee1ea3cecf3e6e08067cc58ec59dec321
Guy Van den Nieuwenhof <guyvdn@gmail.com>
2020-12-02 20:02:17 +0100

    Day02

---
+++ AdventOfCode2020/Day02.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+
+namespace AdventOfCode2020
+{
+    public class Day02
+    {
+        private class Password
+        {
+            public Password(string input)
+            {
+                var inputs = input.Split(' ');
+                var counts = inputs[0].Split('-');
+                MinCount = int.Parse(counts[0]);
+                MaxCount = int.Parse(counts[1]);
+                RequiredCharacter = inputs[1][0];
+                Value = inputs[2];
+            }
+
+            public int MinCount { get; }
+            public int MaxCount { get; }
+            public char RequiredCharacter { get; }
+            public string Value { get; }
+
+            public bool IsValidForPolicy1
+            {
+                get
+                {
+                    var count = Value.Count(x => x == RequiredCharacter);
+                    return count >= MinCount && count <= MaxCount;
+                }
+            }
+
+            public bool IsValidForPolicy2
+            {
+                get
+                {
+                    var found = 0;
+                    found += (Value[MinCount - 1] == RequiredCharacter) ? 1 : 0;
+                    found += (Value[MaxCount - 1] == RequiredCharacter) ? 1 : 0;
+                    return found == 1;
+                }
+            }
+        }
+
+        private Password[] _input;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            var inputText = File.ReadAllText("Day02_input.txt");
+            var inputTextArray = inputText.Split(Environment.NewLine);
+            _input = inputTextArray.Select(input => new Password(input)).ToArray();
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var validCount = _input.Count(x => x.IsValidForPolicy1);
+            TestContext.WriteLine($"{validCount} passwords are valid");
+        }  
+        
+        [Test]
+        public void Part2()
+        {
+            var validCount = _input.Count(x => x.IsValidForPolicy2);
+            TestContext.WriteLine($"{validCount} passwords are valid");
+        }
+    }
+}
