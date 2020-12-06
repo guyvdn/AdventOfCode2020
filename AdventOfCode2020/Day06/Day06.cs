aee6bf2222ac826edd7759a725ece91a5af0bbd6
Guy Van den Nieuwenhof <guyvdn@gmail.com>
2020-12-06 19:33:59 +0100

    Day06

---
+++ AdventOfCode2020/Day06/Day06.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode2020.Day06
+{
+    public class Day06
+    {
+        private string[] _testData;
+        private string[] _input;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _testData = File.ReadAllText("Data/Day06_testdata.txt").Split(Environment.NewLine + Environment.NewLine);
+            _input = File.ReadAllText("Data/Day06_input.txt").Split(Environment.NewLine + Environment.NewLine);
+        }
+
+        [Test]
+        public void Part1WithTestData()
+        {
+            _testData.Length.ShouldBe(5);
+            var questionnaires = _testData.Select(data => new Questionnaire(data));
+            questionnaires.Sum(q => q.NumberOfQuestionsAnsweredYes()).ShouldBe(11);
+        }
+
+
+        [Test]
+        public void Part1()
+        {
+            _testData.Length.ShouldBe(5);
+            var questionnaires = _input.Select(data => new Questionnaire(data));
+            var answer = questionnaires.Sum(q => q.NumberOfQuestionsAnsweredYes());
+            TestContext.WriteLine($"Answer = {answer}");
+        }
+
+        [Test]
+        public void Part2WithTestData()
+        {
+            _testData.Length.ShouldBe(5);
+            var questionnaires = _testData.Select(data => new Questionnaire(data));
+            questionnaires.Sum(q => q.NumberOfQuestionsAnsweredYesByEveryone()).ShouldBe(6);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            _testData.Length.ShouldBe(5);
+            var questionnaires = _input.Select(data => new Questionnaire(data));
+            var answer = questionnaires.Sum(q => q.NumberOfQuestionsAnsweredYesByEveryone());
+            TestContext.WriteLine($"Answer = {answer}");
+        }
+    }
+}
---
+++ AdventOfCode2020/Day06/Questionnaire.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace AdventOfCode2020.Day06
+{
+    public class Questionnaire
+    {
+        private readonly string[] _values;
+
+        public Questionnaire(string values)
+        {
+            _values = values.Split(Environment.NewLine);
+        }
+
+        public int NumberOfQuestionsAnsweredYes()
+        {
+            return string
+                .Join("", _values)
+                .Distinct()
+                .Count();
+        }
+        
+        public int NumberOfQuestionsAnsweredYesByEveryone()
+        {
+            var numberOfPeopleInGroup = _values.Length;
+            return string
+                .Join("", _values)
+                .GroupBy(x => x)
+                .Count(group => group.Count() == numberOfPeopleInGroup);
+        }
+    }
+}
