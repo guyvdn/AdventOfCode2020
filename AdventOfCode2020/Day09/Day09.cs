c1120cda09d56eb588342803baf526c79ff47caf
Guy Van den Nieuwenhof <guyvdn@gmail.com>
2020-12-09 20:22:50 +0100

    Day 09

---
+++ AdventOfCode2020/Day09/Day09.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode2020.Day09
+{
+    public class Day09
+    {
+        private string[] _testData;
+        private string[] _input;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _testData = File.ReadAllText("Data/Day09_testdata.txt").Split(Environment.NewLine);
+            _input = File.ReadAllText("Data/Day09_input.txt").Split(Environment.NewLine);
+        }
+
+        [Test]
+        public void Part1WithTestData()
+        {
+            var xmas = new Xmas(_testData);
+            var invalidNumber = xmas.GetInvalidNumber(5);
+            invalidNumber.ShouldBe(127);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var xmas = new Xmas(_input);
+            var invalidNumber = xmas.GetInvalidNumber(25);
+            TestContext.WriteLine($"Answer = {invalidNumber}");
+        }
+
+        [Test]
+        public void Part2WithTestData()
+        {
+            var xmas = new Xmas(_testData);
+            var weakness = xmas.FindWeakness(127);
+            weakness.ShouldBe(62);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var xmas = new Xmas(_input);
+            var invalidNumber = xmas.GetInvalidNumber(25);
+            var weakness = xmas.FindWeakness(invalidNumber);
+            TestContext.WriteLine($"Answer = {weakness}");
+        }
+    }
+}
---
+++ AdventOfCode2020/Day09/Xmas.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2020.Day09
+{
+    public class Xmas
+    {
+        private readonly long[] _data;
+
+        public Xmas(IEnumerable<string> data)
+        {
+            _data = data.Select(long.Parse).ToArray();
+        }
+
+        public long GetInvalidNumber(int preamble)
+        {
+            for (var index = preamble; index < _data.Length; index++)
+            {
+                var valueToValidate = _data[index];
+                var preambleData = _data.Skip(index - preamble).Take(preamble).ToList();
+                if (!preambleData.Any(x => preambleData.Any(y => x + y == valueToValidate)))
+                    return valueToValidate;
+            }
+
+            return -1;
+        }
+
+        public long FindWeakness(long invalidNumber)
+        {
+            for (var index = 0; index < _data.Length; index++)
+            {
+                var sum = 0L;
+                var numbersToTake = 1;
+                while (sum < invalidNumber)
+                {
+                    var numbers = _data.Skip(index).Take(numbersToTake).ToList();
+                    sum = numbers.Sum();
+                    if (sum == invalidNumber)
+                    {
+                        return numbers.Min() + numbers.Max();
+                    }
+                    numbersToTake++;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
