89e1ee01d6c94ba58708519f46b87f13b1e1bb7b
Guy Van den Nieuwenhof <guyvdn@gmail.com>
2020-12-10 20:13:12 +0100

    Day10

---
+++ AdventOfCode2020/Day10/Adapters.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace AdventOfCode2020.Day10
+{
+    public class Adapters
+    {
+        private readonly List<int> _jolts;
+        private readonly int _maxJolt;
+        private readonly MemoryCache _cache = new(new MemoryCacheOptions());
+
+        public Adapters(IReadOnlyCollection<int> jolts)
+        {
+            _jolts = new List<int> { 0 };
+            _jolts.AddRange(jolts);
+            _maxJolt = jolts.Max() + 3;
+            _jolts.Add(_maxJolt);
+            _jolts = _jolts.OrderBy(jolt => jolt).ToList();
+        }
+
+        public int CountDifference(int jolt)
+        {
+            var count = 0;
+
+            for (var index = 0; index < _jolts.Count - 1; index++)
+            {
+                if (_jolts[index + 1] - _jolts[index] == jolt)
+                    count++;
+            }
+
+            return count;
+        }
+
+        public long CountPossibleCombinations(long startValue = 0)
+        {
+            var count = 0L;
+
+            foreach (var jolt in _jolts.Where(jolt => jolt > startValue && jolt <= startValue + 3))
+            {
+                if (jolt == _maxJolt)
+                    return 1;
+
+                count += _cache.GetOrCreate(jolt, _ => CountPossibleCombinations(jolt));
+            }
+
+            return count;
+        }
+    }
+}
---
+++ AdventOfCode2020/Day10/Day10.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode2020.Day10
+{
+    public class Day10
+    {
+        private IReadOnlyCollection<int> _testData1;
+        private IReadOnlyCollection<int> _testData2;
+        private IReadOnlyCollection<int> _input;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _testData1 = File.ReadAllText("Data/Day10_testdata1.txt").Split(Environment.NewLine).Select(int.Parse).ToList();
+            _testData2 = File.ReadAllText("Data/Day10_testdata2.txt").Split(Environment.NewLine).Select(int.Parse).ToList();
+            _input = File.ReadAllText("Data/Day10_input.txt").Split(Environment.NewLine).Select(int.Parse).ToList();
+        }
+
+        [Test]
+        public void Part1WithTestData1()
+        {
+            var adapters = new Adapters(_testData1);
+            adapters.CountDifference(1).ShouldBe(7);
+            adapters.CountDifference(3).ShouldBe(5);
+        }
+
+        [Test]
+        public void Part1WithTestData2()
+        {
+            var adapters = new Adapters(_testData2);
+            adapters.CountDifference(1).ShouldBe(22);
+            adapters.CountDifference(3).ShouldBe(10);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var adapters = new Adapters(_input);
+            var countDifference1 = adapters.CountDifference(1);
+            var countDifference3 = adapters.CountDifference(3);
+            TestContext.WriteLine($"Answer = {countDifference1 * countDifference3}");
+        }
+
+        [Test]
+        public void Part2WithTestData1()
+        {
+            var adapters = new Adapters(_testData1);
+            adapters.CountPossibleCombinations().ShouldBe(8);
+        }
+
+        [Test]
+        public void Part2WithTestData2()
+        {
+            var adapters = new Adapters(_testData2);
+            adapters.CountPossibleCombinations().ShouldBe(19208);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var adapters = new Adapters(_input);
+            TestContext.WriteLine($"Answer = {adapters.CountPossibleCombinations()}");
+        }
+    }
+}
