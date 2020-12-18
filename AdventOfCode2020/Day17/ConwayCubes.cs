98107a3b4cb9c60f8bf11edb297fb935b70d69f7
Guy Van den Nieuwenhof <guyvdn@gmail.com>
2020-12-18 16:58:16 +0100

    Day17

---
+++ AdventOfCode2020/Day17/ConwayCubes.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AdventOfCode2020.Day17
+{
+    public class ConwayCubes
+    {
+        private HashSet<string> _activeCubes = new();
+        private int _minX;
+        private int _maxX;
+        private int _minY;
+        private int _maxY;
+        private int _minZ;
+        private int _maxZ;
+        private int _minW;
+        private int _maxW;
+
+        public ConwayCubes(IReadOnlyList<string> input)
+        {
+            _maxX = input.Count;
+            _maxY = input.Count;
+            _maxZ = 1;
+            _maxW = 1;
+
+            for (var x = _minX; x < _maxX; x++)
+                for (var y = _minY; y < _maxY; y++)
+                    if (input[y][x] == '#')
+                        _activeCubes.Add(PositionToKey(x, y, 0, 0));
+        }
+
+        private static string PositionToKey(in int x, in int y, in int z, in int w)
+        {
+            return $"{x}|{y}|{z}|{w}";
+        }
+
+        public int ActiveCubes => _activeCubes.Count;
+
+        public void Cycle3D(int times)
+        {
+            Cycle(times, Expand3D, Activate3D);
+        }
+
+        public void Cycle4D(int times)
+        {
+            Cycle(times, Expand4D, Activate4D);
+        }
+
+        private void Cycle(int times, Action expandFunction, Func<int, int, int, int, bool> activateFunction)
+        {
+            for (var time = 1; time <= times; time++)
+            {
+                expandFunction();
+
+                var newState = new HashSet<string>();
+
+                for (var x = _minX; x < _maxX; x++)
+                    for (var y = _minY; y < _maxY; y++)
+                        for (var z = _minZ; z < _maxZ; z++)
+                            for (var w = _minW; w < _maxW; w++)
+                                if (activateFunction(x, y, z, w))
+                                    newState.Add(PositionToKey(x, y, z, w));
+
+                _activeCubes = newState;
+            }
+        }
+
+        private bool Activate3D(int x, int y, int z, int w)
+        {
+            var activeNeighbours = 0;
+
+            for (var nX = -1; nX <= 1; nX++)
+                for (var nY = -1; nY <= 1; nY++)
+                    for (var nZ = -1; nZ <= 1; nZ++)
+                        if (_activeCubes.Contains(PositionToKey(x + nX, y + nY, z + nZ, w)))
+                            activeNeighbours++;
+
+            return activeNeighbours == 3 || (activeNeighbours == 4 && _activeCubes.Contains(PositionToKey(x, y, z, w)));
+        }
+
+        private bool Activate4D(int x, int y, int z, int w)
+        {
+            var activeNeighbours = 0;
+
+            for (var nX = -1; nX <= 1; nX++)
+                for (var nY = -1; nY <= 1; nY++)
+                    for (var nZ = -1; nZ <= 1; nZ++)
+                        for (var nW = -1; nW <= 1; nW++)
+                            if (_activeCubes.Contains(PositionToKey(x + nX, y + nY, z + nZ, w + nW)))
+                                activeNeighbours++;
+
+            return activeNeighbours == 3 || (activeNeighbours == 4 && _activeCubes.Contains(PositionToKey(x, y, z, w)));
+        }
+
+        private void Expand3D()
+        {
+            _minX--;
+            _maxX++;
+            _minY--;
+            _maxY++;
+            _minZ--;
+            _maxZ++;
+        }
+
+        private void Expand4D()
+        {
+            Expand3D();
+            _minW--;
+            _maxW++;
+        }
+    }
+}
---
+++ AdventOfCode2020/Day17/Day17.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+using Shouldly;
+
+namespace AdventOfCode2020.Day17
+{
+    public class Day17
+    {
+        private string[] _testData;
+        //private string[] _testData2;
+        private string[] _input;
+
+        [OneTimeSetUp]
+        public void SetUp()
+        {
+            _testData = File.ReadAllText("Data/Day17_testdata.txt").Split(Environment.NewLine);
+            _input = File.ReadAllText("Data/Day17_input.txt").Split(Environment.NewLine);
+        }
+
+        [Test]
+        public void Part1WithTestData()
+        {
+            var conwayCubes = new ConwayCubes(_testData);
+            conwayCubes.ActiveCubes.ShouldBe(5);
+            conwayCubes.Cycle3D(1);
+            conwayCubes.ActiveCubes.ShouldBe(11);
+            conwayCubes.Cycle3D(5);
+            conwayCubes.ActiveCubes.ShouldBe(112);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var conwayCubes = new ConwayCubes(_input);
+            conwayCubes.Cycle3D(6);
+            var answer = conwayCubes.ActiveCubes;
+            TestContext.WriteLine($"Answer = {answer}");
+        }
+
+        [Test]
+        public void Part2WithTestData()
+        {
+            var conwayCubes = new ConwayCubes(_testData);
+            conwayCubes.Cycle4D(6);
+            conwayCubes.ActiveCubes.ShouldBe(848);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var conwayCubes = new ConwayCubes(_input);
+            conwayCubes.Cycle4D(6);
+            var answer = conwayCubes.ActiveCubes;
+            TestContext.WriteLine($"Answer = {answer}");
+        }
+    }
+}
